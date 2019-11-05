c7304d6f9cce1e838d76b3ca0e047b5d628928c7
gabighiunghius <56878112+gabighiunghius@users.noreply.github.com>
2019-11-05 16:10:25 +0200

    Add files via upload

---
+++ MyQueue.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sapt4_Tema2
+{
+    public class MyQueue
+    {
+        public int[] a = new int[10];
+        public int l = 0;
+        public int m = 0;
+        public void Enqueue(int e)
+        {
+            int f = a.Length;
+            if (f == a.Length)
+            {
+                Console.WriteLine("The queue is full");
+            }
+            else
+                a[l] = e;
+            l++;
+        }
+        public int top()
+        {
+            int e;
+            e = a[m];
+            a[m] = 0;
+            return e;
+        }
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sapt4_Tema2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            //Stack myStack = new Stack();
+
+            //myStack.Push(10);
+            //myStack.Push(20);
+            //myStack.Push(30);
+            //myStack.Push(40);
+
+
+            //foreach (int i in myStack)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+
+            //myStack.Peek();
+            //Console.WriteLine("Item popped from Stack : {0}", myStack.Pop());
+
+            //foreach (int i in myStack)
+            //{
+            //    Console.WriteLine(i);
+            //}
+            //Console.ReadLine();
+
+            // Create a queue 
+            // Using Queue class 
+            Queue my_queue = new Queue();
+
+            // Adding elements in Queue 
+            // Using Enqueue() method 
+            my_queue.Enqueue("GFG");
+            my_queue.Enqueue("Geeks");
+            my_queue.Enqueue("GeeksforGeeks");
+            my_queue.Enqueue("geeks");
+            my_queue.Enqueue("Geeks123");
+
+
+        }
+    }
+}
